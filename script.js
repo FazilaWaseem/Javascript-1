cbcd2e66cb1416ca7e0433b5cdf0427cd1a65ed2
Fazila Waseem <fazilawaseem829@gmail.com>
2024-08-13 18:53:16 +0500

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+//Question 1
+alert("I am 23 Years old" );
+//Question 3
+var myBirthYear = 2001;
+document.write(" <br>My birth year is" + " " +myBirthYear + "<br/>");
+console.log(myBirthYear);
+document.write(" <br>Data type of my declared variable is number" +"<br/>");
+ 
+//Question 2
+alert("You have visited this site 14 times");
+
+//Question 4
+var visitorsName = " <br> <b> Jhon Doe </b>";
+document.write(visitorsName);
+console.log(visitorsName);
+
+var productTitle = "ordered";
+document.write(productTitle);
+console.log(productTitle);
+ 
+var quantity = " <b>5T-shirt</b>(s)";
+document.write(quantity);
+console.log(quantity);
+
+var clothingStore = "on XYZ Clothing store";
+document.write(clothingStore);
+console.log(clothingStore);
+
+
+
